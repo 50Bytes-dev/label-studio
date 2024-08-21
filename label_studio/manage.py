3118cfff14b103dc44401dc9986bf97021dec9e6
50bytes.dev <50bytes.dev@gmail.com>
2024-08-21 13:16:18 +0400

    fix

--- label_studio/manage.py
+++ label_studio/manage.py
@@ -4,8 +4,8 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.label_studio')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.label_studio")
     # os.environ.setdefault('DEBUG', 'True')
     try:
         from django.conf import settings
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            'available on your PYTHONPATH environment variable? Did you '
-            'forget to activate a virtual environment?'
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
