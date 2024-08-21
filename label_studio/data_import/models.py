289c090b42c3e65db5b29608b3d11294962b32ef
50bytes.dev <50bytes.dev@gmail.com>
2024-08-21 11:51:16 +0400

    add jsonl

--- label_studio/data_import/models.py
+++ label_studio/data_import/models.py
@@ -1,5 +1,6 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+
 import logging
 import os
 import uuid
@@ -24,13 +25,25 @@ def upload_name_generator(instance, filename):
     project = str(instance.project_id)
     project_dir = os.path.join(settings.MEDIA_ROOT, settings.UPLOAD_DIR, project)
     os.makedirs(project_dir, exist_ok=True)
-    path = settings.UPLOAD_DIR + '/' + project + '/' + str(uuid.uuid4())[0:8] + '-' + filename
+    path = (
+        settings.UPLOAD_DIR
+        + "/"
+        + project
+        + "/"
+        + str(uuid.uuid4())[0:8]
+        + "-"
+        + filename
+    )
     return path
 
 
 class FileUpload(models.Model):
-    user = models.ForeignKey('users.User', related_name='file_uploads', on_delete=models.CASCADE)
-    project = models.ForeignKey('projects.Project', related_name='file_uploads', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User", related_name="file_uploads", on_delete=models.CASCADE
+    )
+    project = models.ForeignKey(
+        "projects.Project", related_name="file_uploads", on_delete=models.CASCADE
+    )
     file = models.FileField(upload_to=upload_name_generator)
 
     def has_permission(self, user):
@@ -44,12 +57,15 @@ class FileUpload(models.Model):
     @property
     def url(self):
         if settings.HOSTNAME and settings.CLOUD_FILE_STORAGE_ENABLED:
-            if flag_set('ff_back_dev_2915_storage_nginx_proxy_26092022_short', self.project.organization.created_by):
+            if flag_set(
+                "ff_back_dev_2915_storage_nginx_proxy_26092022_short",
+                self.project.organization.created_by,
+            ):
                 return self.file.url
             else:
                 return settings.HOSTNAME + self.file.url
         elif settings.FORCE_SCRIPT_NAME:
-            return settings.FORCE_SCRIPT_NAME + '/' + self.file.url.lstrip('/')
+            return settings.FORCE_SCRIPT_NAME + "/" + self.file.url.lstrip("/")
         else:
             return self.file.url
 
@@ -62,105 +78,132 @@ class FileUpload(models.Model):
         except:  # noqa: E722
             pass
         finally:
-            logger.debug('Get file format ' + str(file_format))
+            logger.debug("Get file format " + str(file_format))
         return file_format
 
     @property
     def content(self):
         # cache file body
-        if hasattr(self, '_file_body'):
-            body = getattr(self, '_file_body')
+        if hasattr(self, "_file_body"):
+            body = getattr(self, "_file_body")
         else:
-            body = self.file.read().decode('utf-8')
-            setattr(self, '_file_body', body)
+            body = self.file.read().decode("utf-8")
+            setattr(self, "_file_body", body)
         return body
 
-    def read_tasks_list_from_csv(self, sep=','):
-        logger.debug('Read tasks list from CSV file {}'.format(self.file.name))
-        tasks = pd.read_csv(self.file.open(), sep=sep).fillna('').to_dict('records')
-        tasks = [{'data': task} for task in tasks]
+    def read_tasks_list_from_csv(self, sep=","):
+        logger.debug("Read tasks list from CSV file {}".format(self.file.name))
+        tasks = pd.read_csv(self.file.open(), sep=sep).fillna("").to_dict("records")
+        tasks = [{"data": task} for task in tasks]
         return tasks
 
     def read_tasks_list_from_tsv(self):
-        return self.read_tasks_list_from_csv('\t')
+        return self.read_tasks_list_from_csv("\t")
 
     def read_tasks_list_from_txt(self):
-        logger.debug('Read tasks list from text file {}'.format(self.file.name))
+        logger.debug("Read tasks list from text file {}".format(self.file.name))
         lines = self.content.splitlines()
-        tasks = [{'data': {settings.DATA_UNDEFINED_NAME: line}} for line in lines]
+        tasks = [{"data": {settings.DATA_UNDEFINED_NAME: line}} for line in lines]
         return tasks
 
     def read_tasks_list_from_json(self):
-        logger.debug('Read tasks list from JSON file {}'.format(self.file.name))
+        logger.debug("Read tasks list from JSON file {}".format(self.file.name))
 
         raw_data = self.content
         # Python 3.5 compatibility fix https://docs.python.org/3/whatsnew/3.6.html#json
         try:
             tasks = json.loads(raw_data)
         except TypeError:
-            tasks = json.loads(raw_data.decode('utf8'))
+            tasks = json.loads(raw_data.decode("utf8"))
         if isinstance(tasks, dict):
             tasks = [tasks]
         tasks_formatted = []
         for i, task in enumerate(tasks):
-            if not task.get('data'):
-                task = {'data': task}
-            if not isinstance(task['data'], dict):
-                raise ValidationError('Task item should be dict')
+            if not task.get("data"):
+                task = {"data": task}
+            if not isinstance(task["data"], dict):
+                raise ValidationError("Task item should be dict")
             tasks_formatted.append(task)
         return tasks_formatted
 
+    def read_tasks_list_from_jsonl(self):
+        logger.debug("Read tasks list from JSONL file {}".format(self.file.name))
+
+        raw_data: str = self.content
+        tasks = []
+        for line in raw_data.splitlines():
+            try:
+                task = json.loads(line)
+            except TypeError:
+                task = json.loads(line.decode("utf8"))
+            if not task.get("data"):
+                task = {"data": task}
+            if not isinstance(task["data"], dict):
+                raise ValidationError("Task item should be dict")
+            tasks.append(task)
+
+        return tasks
+
     def read_task_from_hypertext_body(self):
-        logger.debug('Read 1 task from hypertext file {}'.format(self.file.name))
+        logger.debug("Read 1 task from hypertext file {}".format(self.file.name))
         body = self.content
-        tasks = [{'data': {settings.DATA_UNDEFINED_NAME: body}}]
+        tasks = [{"data": {settings.DATA_UNDEFINED_NAME: body}}]
         return tasks
 
     def read_task_from_uploaded_file(self):
-        logger.debug('Read 1 task from uploaded file {}'.format(self.file.name))
+        logger.debug("Read 1 task from uploaded file {}".format(self.file.name))
         if settings.CLOUD_FILE_STORAGE_ENABLED:
-            tasks = [{'data': {settings.DATA_UNDEFINED_NAME: self.file.name}}]
+            tasks = [{"data": {settings.DATA_UNDEFINED_NAME: self.file.name}}]
         else:
-            tasks = [{'data': {settings.DATA_UNDEFINED_NAME: self.url}}]
+            tasks = [{"data": {settings.DATA_UNDEFINED_NAME: self.url}}]
         return tasks
 
     @property
     def format_could_be_tasks_list(self):
-        return self.format in ('.csv', '.tsv', '.txt')
+        return self.format in (".csv", ".tsv", ".txt")
 
     def read_tasks(self, file_as_tasks_list=True):
         file_format = self.format
         try:
             # file as tasks list
-            if file_format == '.csv' and file_as_tasks_list:
+            if file_format == ".csv" and file_as_tasks_list:
                 tasks = self.read_tasks_list_from_csv()
-            elif file_format == '.tsv' and file_as_tasks_list:
+            elif file_format == ".tsv" and file_as_tasks_list:
                 tasks = self.read_tasks_list_from_tsv()
-            elif file_format == '.txt' and file_as_tasks_list:
+            elif file_format == ".txt" and file_as_tasks_list:
                 tasks = self.read_tasks_list_from_txt()
-            elif file_format == '.json':
+            elif file_format == ".json":
                 tasks = self.read_tasks_list_from_json()
+            elif file_format == ".jsonl":
+                tasks = self.read_tasks_list_from_jsonl()
 
             # otherwise - only one object tag should be presented in label config
             elif not self.project.one_object_in_label_config:
                 raise ValidationError(
-                    'Your label config has more than one data key and direct file upload supports only '
-                    'one data key. To import data with multiple data keys, use a JSON or CSV file.'
+                    "Your label config has more than one data key and direct file upload supports only "
+                    "one data key. To import data with multiple data keys, use a JSON or CSV file."
                 )
 
             # file as a single asset
-            elif file_format in ('.html', '.htm', '.xml'):
+            elif file_format in (".html", ".htm", ".xml"):
                 tasks = self.read_task_from_hypertext_body()
             else:
                 tasks = self.read_task_from_uploaded_file()
 
         except Exception as exc:
-            raise ValidationError('Failed to parse input file ' + self.file.name + ': ' + str(exc))
+            raise ValidationError(
+                "Failed to parse input file " + self.file.name + ": " + str(exc)
+            )
         return tasks
 
     @classmethod
     def load_tasks_from_uploaded_files(
-        cls, project, file_upload_ids=None, formats=None, files_as_tasks_list=True, trim_size=None
+        cls,
+        project,
+        file_upload_ids=None,
+        formats=None,
+        files_as_tasks_list=True,
+        trim_size=None,
     ):
         tasks = []
         fileformats = []
@@ -176,9 +219,11 @@ class FileUpload(models.Model):
                 continue
             new_tasks = file_upload.read_tasks(files_as_tasks_list)
             for task in new_tasks:
-                task['file_upload_id'] = file_upload.id
+                task["file_upload_id"] = file_upload.id
 
-            new_data_fields = set(iter(new_tasks[0]['data'].keys())) if len(new_tasks) > 0 else set()
+            new_data_fields = (
+                set(iter(new_tasks[0]["data"].keys())) if len(new_tasks) > 0 else set()
+            )
             if not common_data_fields:
                 common_data_fields = new_data_fields
             elif not common_data_fields.intersection(new_data_fields):
@@ -200,21 +245,27 @@ class FileUpload(models.Model):
         return tasks, dict(Counter(fileformats)), common_data_fields
 
 
-def _old_vs_new_data_keys_inconsistency_message(new_data_keys, old_data_keys, current_file):
-    new_data_keys_list = ','.join(new_data_keys)
-    old_data_keys_list = ','.join(old_data_keys)
+def _old_vs_new_data_keys_inconsistency_message(
+    new_data_keys, old_data_keys, current_file
+):
+    new_data_keys_list = ",".join(new_data_keys)
+    old_data_keys_list = ",".join(old_data_keys)
     common_prefix = "You're trying to import inconsistent data:\n"
     if new_data_keys_list == old_data_keys_list:
-        return ''
+        return ""
     elif new_data_keys_list == settings.DATA_UNDEFINED_NAME:
         return (
-            common_prefix + 'uploading a single file {0} '
-            'clashes with data key(s) found from other files:\n"{1}"'.format(current_file, old_data_keys_list)
+            common_prefix + "uploading a single file {0} "
+            'clashes with data key(s) found from other files:\n"{1}"'.format(
+                current_file, old_data_keys_list
+            )
         )
     elif old_data_keys_list == settings.DATA_UNDEFINED_NAME:
         return (
-            common_prefix + 'uploading tabular data from {0} with data key(s) {1}, '
-            'clashes with other raw binary files (images, audios, etc.)'.format(current_file, new_data_keys_list)
+            common_prefix + "uploading tabular data from {0} with data key(s) {1}, "
+            "clashes with other raw binary files (images, audios, etc.)".format(
+                current_file, new_data_keys_list
+            )
         )
     else:
         return (
--- label_studio/data_import/uploader.py
+++ label_studio/data_import/uploader.py
@@ -1,5 +1,6 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+
 import csv
 import io
 import logging
@@ -33,9 +34,9 @@ def csv_generate_header(file):
     names = []
     line = file.readline()
 
-    num_columns = len(line.split(b',' if isinstance(line, bytes) else ','))
+    num_columns = len(line.split(b"," if isinstance(line, bytes) else ","))
     for i in range(num_columns):
-        names.append('column' + str(i + 1))
+        names.append("column" + str(i + 1))
     file.seek(0)
     return names
 
@@ -44,15 +45,16 @@ def check_max_task_number(tasks):
     # max tasks
     if len(tasks) > settings.TASKS_MAX_NUMBER:
         raise ValidationError(
-            f'Maximum task number is {settings.TASKS_MAX_NUMBER}, ' f'current task number is {len(tasks)}'
+            f"Maximum task number is {settings.TASKS_MAX_NUMBER}, "
+            f"current task number is {len(tasks)}"
         )
 
 
 def check_tasks_max_file_size(value):
     if value >= settings.TASKS_MAX_FILE_SIZE:
         raise ValidationError(
-            f'Maximum total size of all files is {settings.TASKS_MAX_FILE_SIZE} bytes, '
-            f'current size is {value} bytes'
+            f"Maximum total size of all files is {settings.TASKS_MAX_FILE_SIZE} bytes, "
+            f"current size is {value} bytes"
         )
 
 
@@ -60,7 +62,7 @@ def check_extensions(files):
     for filename, file_obj in files.items():
         _, ext = os.path.splitext(file_obj.name)
         if ext.lower() not in settings.SUPPORTED_EXTENSIONS:
-            raise ValidationError(f'{ext} extension is not supported')
+            raise ValidationError(f"{ext} extension is not supported")
 
 
 def check_request_files_size(files):
@@ -73,7 +75,7 @@ def create_file_upload(user, project, file):
     instance = FileUpload(user=user, project=project, file=file)
     if settings.SVG_SECURITY_CLEANUP:
         content_type, encoding = mimetypes.guess_type(str(instance.file.name))
-        if content_type in ['image/svg+xml']:
+        if content_type in ["image/svg+xml"]:
             clean_xml = allowlist_svg(instance.file.read())
             instance.file.seek(0)
             instance.file.write(clean_xml)
@@ -89,15 +91,15 @@ def allowlist_svg(dirty_xml):
     from lxml.html import clean
 
     allow_tags = [
-        'xml',
-        'svg',
-        'circle',
-        'ellipse',
-        'line',
-        'path',
-        'polygon',
-        'polyline',
-        'rect',
+        "xml",
+        "svg",
+        "circle",
+        "ellipse",
+        "line",
+        "path",
+        "polygon",
+        "polyline",
+        "rect",
     ]
 
     cleaner = clean.Cleaner(
@@ -126,18 +128,25 @@ def tasks_from_url(file_upload_ids, project, user, url, could_be_tasks_list):
     """Download file using URL and read tasks from it"""
     # process URL with tasks
     try:
-        filename = url.rsplit('/', 1)[-1]
+        filename = url.rsplit("/", 1)[-1]
 
         response = ssrf_safe_get(
-            url, verify=project.organization.should_verify_ssl_certs(), stream=True, headers={'Accept-Encoding': None}
+            url,
+            verify=project.organization.should_verify_ssl_certs(),
+            stream=True,
+            headers={"Accept-Encoding": None},
         )
         file_content = response.content
-        check_tasks_max_file_size(int(response.headers['content-length']))
-        file_upload = create_file_upload(user, project, SimpleUploadedFile(filename, file_content))
+        check_tasks_max_file_size(int(response.headers["content-length"]))
+        file_upload = create_file_upload(
+            user, project, SimpleUploadedFile(filename, file_content)
+        )
         if file_upload.format_could_be_tasks_list:
             could_be_tasks_list = True
         file_upload_ids.append(file_upload.id)
-        tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(project, file_upload_ids)
+        tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(
+            project, file_upload_ids
+        )
 
     except ValidationError as e:
         raise e
@@ -158,7 +167,9 @@ def create_file_uploads(user, project, FILES):
             could_be_tasks_list = True
         file_upload_ids.append(file_upload.id)
 
-    logger.debug(f'created file uploads: {file_upload_ids} could_be_tasks_list: {could_be_tasks_list}')
+    logger.debug(
+        f"created file uploads: {file_upload_ids} could_be_tasks_list: {could_be_tasks_list}"
+    )
     return file_upload_ids, could_be_tasks_list
 
 
@@ -181,7 +192,7 @@ def load_tasks_for_async_import(project_import, user):
             file_upload = create_file_upload(
                 user,
                 project_import.project,
-                SimpleUploadedFile('inplace.json', url.encode()),
+                SimpleUploadedFile("inplace.json", url.encode()),
             )
             file_upload_ids.append(file_upload.id)
             tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(
@@ -197,21 +208,23 @@ def load_tasks_for_async_import(project_import, user):
                 tasks,
                 file_upload_ids,
                 could_be_tasks_list,
-            ) = tasks_from_url(file_upload_ids, project_import.project, user, url, could_be_tasks_list)
+            ) = tasks_from_url(
+                file_upload_ids, project_import.project, user, url, could_be_tasks_list
+            )
             if could_be_tasks_list:
                 project_import.could_be_tasks_list = True
-                project_import.save(update_fields=['could_be_tasks_list'])
+                project_import.save(update_fields=["could_be_tasks_list"])
 
     elif project_import.tasks:
         tasks = project_import.tasks
 
     # check is data root is list
     if not isinstance(tasks, list):
-        raise ValidationError('load_tasks: Data root must be list')
+        raise ValidationError("load_tasks: Data root must be list")
 
     # empty tasks error
     if not tasks:
-        raise ValidationError('load_tasks: No tasks added')
+        raise ValidationError("load_tasks: No tasks added")
 
     check_max_task_number(tasks)
     return tasks, file_upload_ids, found_formats, list(data_keys)
@@ -231,21 +244,27 @@ def load_tasks(request, project):
             if file_upload.format_could_be_tasks_list:
                 could_be_tasks_list = True
             file_upload_ids.append(file_upload.id)
-        tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(project, file_upload_ids)
+        tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(
+            project, file_upload_ids
+        )
 
     # take tasks from url address
-    elif 'application/x-www-form-urlencoded' in request.content_type:
+    elif "application/x-www-form-urlencoded" in request.content_type:
         # empty url
-        url = request.data.get('url')
+        url = request.data.get("url")
         if not url:
             raise ValidationError('"url" is not found in request data')
 
         # try to load json with task or tasks from url as string
         json_data = str_to_json(url)
         if json_data:
-            file_upload = create_file_upload(request.user, project, SimpleUploadedFile('inplace.json', url.encode()))
+            file_upload = create_file_upload(
+                request.user, project, SimpleUploadedFile("inplace.json", url.encode())
+            )
             file_upload_ids.append(file_upload.id)
-            tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(project, file_upload_ids)
+            tasks, found_formats, data_keys = FileUpload.load_tasks_from_uploaded_files(
+                project, file_upload_ids
+            )
 
         # download file using url and read tasks from it
         else:
@@ -255,27 +274,29 @@ def load_tasks(request, project):
                 tasks,
                 file_upload_ids,
                 could_be_tasks_list,
-            ) = tasks_from_url(file_upload_ids, project, request.user, url, could_be_tasks_list)
+            ) = tasks_from_url(
+                file_upload_ids, project, request.user, url, could_be_tasks_list
+            )
 
     # take one task from request DATA
-    elif 'application/json' in request.content_type and isinstance(request.data, dict):
+    elif "application/json" in request.content_type and isinstance(request.data, dict):
         tasks = [request.data]
 
     # take many tasks from request DATA
-    elif 'application/json' in request.content_type and isinstance(request.data, list):
+    elif "application/json" in request.content_type and isinstance(request.data, list):
         tasks = request.data
 
     # incorrect data source
     else:
-        raise ValidationError('load_tasks: No data found in DATA or in FILES')
+        raise ValidationError("load_tasks: No data found in DATA or in FILES")
 
     # check is data root is list
     if not isinstance(tasks, list):
-        raise ValidationError('load_tasks: Data root must be list')
+        raise ValidationError("load_tasks: Data root must be list")
 
     # empty tasks error
     if not tasks:
-        raise ValidationError('load_tasks: No tasks added')
+        raise ValidationError("load_tasks: No tasks added")
 
     check_max_task_number(tasks)
     return tasks, file_upload_ids, could_be_tasks_list, found_formats, list(data_keys)
